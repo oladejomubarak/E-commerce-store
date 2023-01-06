68f527bdbd40a2d2ee773ca8f3f4f7ba39a08919
oladejomubarak <oladejomubarakade@gmail.com>
2023-01-06 08:07:34 +0000

    user exception updated

--- src/main/java/store/exception/BuyerRegistrationException.java
+++ src/main/java/store/exception/BuyerRegistrationException.java
@@ -1,7 +1,7 @@
 package store.exception;
 
 public class BuyerRegistrationException extends RuntimeException {
-    public BuyerRegistrationException(String s) {
-        super(s);
+    public BuyerRegistrationException(String message) {
+        super(message);
     }
 }
